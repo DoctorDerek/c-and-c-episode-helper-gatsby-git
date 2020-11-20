bd5c1a87ebd5208419095faac830ec75732fd0bc
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-20 16:26:53 -0500

    Prettier

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,19 @@
-import Branding from './Branding';
-import Footer from './Footer';
-import FormField from './FormField';
-import Header from './Header';
-import HeaderAlt from './HeaderAlt';
-import Navigation from './Navigation';
-import Layout from './Layout';
+import Branding from "./Branding"
+import Footer from "./Footer"
+import FormField from "./FormField"
+import Header from "./Header"
+import HeaderAlt from "./HeaderAlt"
+import Navigation from "./Navigation"
+import Layout from "./Layout"
 
-export {
-    Branding,
-    Footer,
-    FormField,
-    Header,
-    HeaderAlt,
-    Navigation,
-    Layout
-};
+export { Branding, Footer, FormField, Header, HeaderAlt, Navigation, Layout }
 
 export default {
-    Branding,
-    Footer,
-    FormField,
-    Header,
-    HeaderAlt,
-    Navigation,
-    Layout
-};
+  Branding,
+  Footer,
+  FormField,
+  Header,
+  HeaderAlt,
+  Navigation,
+  Layout,
+}
