1f399e5a460729a334becf0a3080ba63e6284ee0
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-23 16:12:58 -0500

    Added  C&C  components to post page

---
+++ src/components/chillhop-and-code/index.js
@@ -0,0 +1,24 @@
+import ADDHighlights from "./ADDHighlights.component"
+import DateHeading from "./DateHeading.component"
+import EpisodeDescription from "./EpisodeDescription.component"
+import EpisodeHighlights from "./EpisodeHighlights.component"
+import TwitchLiveShow from "./TwitchLiveShow.component"
+import YouTubeTutorial from "./YouTubeTutorial.component"
+
+export {
+  ADDHighlights,
+  DateHeading,
+  EpisodeDescription,
+  EpisodeHighlights,
+  TwitchLiveShow,
+  YouTubeTutorial,
+}
+
+export default {
+  ADDHighlights,
+  DateHeading,
+  EpisodeDescription,
+  EpisodeHighlights,
+  TwitchLiveShow,
+  YouTubeTutorial,
+}
