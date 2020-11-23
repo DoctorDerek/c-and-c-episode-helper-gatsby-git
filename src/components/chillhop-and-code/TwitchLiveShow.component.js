aff41f6f84f2b8b4415234e3e11634b9b311e6d2
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-23 16:57:13 -0500

    Removed some <br/>

--- src/components/chillhop-and-code/TwitchLiveShow.component.js
+++ src/components/chillhop-and-code/TwitchLiveShow.component.js
@@ -2,83 +2,64 @@ import React from "react"
 
 const TwitchLiveShow = () => (
   <section>
-    <h2>
-      <br />
-      TWITCH LIVE SHOW
-      <br />
-    </h2>
-    <br />
-    <h3>
-      Live Twitch chapter-based categories & titles (140 characters):
-      <br />
-    </h3>
-    <br />
+    <h2>TWITCH LIVE SHOW</h2>
+
+    <h3>Live Twitch chapter-based categories & titles (140 characters):</h3>
+
     <h4 key="Label for Hour 0">
       Hour 0 Pre-Presentation: Science & Technology
-      <br />
     </h4>
-    <br />
+
     <h5 key="Title for Hour 0">
       💙 Chillhop & Code 📺 JavaScript tutorials, coursework & live programming
       over jazzy / lo-fi beats to relax / study to 👉 DoctorDerek.com 👈
-      <br />
     </h5>
-    <br />
-    <h4 key="Label for Hour 1">
-      Hour 1 Ask Doctor Derek: Just Chatting
-      <br />
-    </h4>
-    <br />
+
+    <h4 key="Label for Hour 1">Hour 1 Ask Doctor Derek: Just Chatting</h4>
+
     <h5 key="Title for Hour 1">
       👨‍⚕️💬🧠 Why I LOVE Fluoride But Drink Filtered Water ❓ Ask Doctor Derek 💪
       Health & Coding 🙋‍♂️ Ask me anything 💙 Chillhop & Code 📺
-      <br />
     </h5>
-    <br />
+
     <h4 key="Label for Hour 2">
       Hours 2 & 6 Let's Learn: Science & Technology
-      <br />
     </h4>
-    <br />
+
     <h5 key="Title for Hour 2">
       🏫💻🎓 Commentary on: Complete React Developer by @AndreiNeagoie 💯 Let's
       Learn: JavaScript Coursework + Commentary 💙 Chillhop & Code 📺 relaxing
       programming
-      <br />
     </h5>
-    <br />
-    <h4 key="Label for Hour 3">
-      Hours 3 & 7 Let's Blog: Just Chatting
-      <br />
-    </h4>
-    <br />
+
+    <h4 key="Label for Hour 3">Hours 3 & 7 Let's Blog: Just Chatting</h4>
+
     <h5 key="Title for Hour 3">
       👨‍💻📝📘 Tour of Articles about Technology by Doctor Derek ✍ Let's Blog:
       JavaScript Tutorials 💙 Chillhop & Code 📺 relaxing programming
-      <br />
     </h5>
-    <br />
+
     <h4 key="Label for Hour 4">
       Hours 4 & 8 Let's Play: Screeps (or Science & Technology)
     </h4>
-    <br />
+
     <h5 key="Title for Hour 4">
       👾🎮🎰 Let's Play: Sourcery & Nester (HTML & Emmet Games) 🎯 Let's Play:
       JavaScript & CSS Games 💙 Chillhop & Code 📺 relaxing programming
     </h5>
-    <br />
+
     <h4 key="Label for Hour 5">Hour 5 Let's Build Websites: Just Chatting</h4>
-    <br />
+
     <h5 key="Title for Hour 5">
       🏗️🕸🔥 Let's Build: A Jamstack Site for My Episode Descriptions 💸 Let's
       Build Websites: Real Client Work 💙 Chillhop & Code 📺 relaxing
       programming
     </h5>
-    <br />
+
     <h4 key="Label for Hour 9">
       Hour 9 Post-Presentation: Science & Technology
     </h4>
-    <br />
+
     <h5 key="Title for Hour 9">
       💙 Chillhop & Code 📺 JavaScript tutorials, coursework & live programming
       over jazzy / lo-fi beats to relax / study to 👉 DoctorDerek.com 👈
